64024bf2a2fdb0b94fd04a21dcc535893bca25a1
harshit9715 <harshit9715@gmail.com>
2020-06-25 18:29:21 +0000

    add code in files types: ,.js,.json,.yml,.state,.gradle,.bzl,.pro,.xml,.java,.properties,.bat,.plist,.pbxproj,.xcscheme,.h,.m,.xib,.svg remove code in files types:

--- content.java
+++ content.java
@@ -1,5 +1,6 @@
 public class Ccksxk {
     public static void main() {
         System.out.println("vunel");
+        System.out.println("qkuai");
     }
 }
--- content.js
+++ content.js
@@ -1,1 +1,2 @@
 console.log("dibpv")
+console.log("tjqwt")
